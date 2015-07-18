5c4c42c63cef2c011ac011f1f613f0f8fd234de6
Nadea <nefr@tut.by>
2015-07-18 23:55:03 +0200

    web test up and running

--- web/src/test/java/com/setplex/middleware/AbstractTestWeb.java
+++ web/src/test/java/com/setplex/middleware/AbstractTestWeb.java
@@ -1,9 +1,11 @@
 package com.setplex.middleware;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -12,8 +14,12 @@ import org.springframework.web.context.WebApplicationContext;
 /**
  * @author Ruslan Yatsevich
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring-web.xml")
+@ContextConfiguration(locations = {
+        "classpath:spring-web.xml",
+        "classpath:spring-ds.xml",
+        "classpath:spring-persistence.xml"})
 public abstract class AbstractTestWeb /*extends AbstractTestConfig */{
     public static final MediaType APPLICATION_JSON_UTF8 = MediaType.parseMediaType("application/json;charset=UTF-8");
 //            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
