be0c094b255ff82ade127f3aa3d8ed4141ff570c
genieyr <ruslan.yatsevichy@gmail.com>
2015-08-04 20:53:24 +0300

    packages with dependencies

--- persistence/src/main/java/com/setplex/middleware/repository/PackageRepo.java
+++ persistence/src/main/java/com/setplex/middleware/repository/PackageRepo.java
@@ -1,7 +1,13 @@
 package com.setplex.middleware.repository;
 
+import com.setplex.middleware.domain.Package;
 import org.springframework.data.jpa.repository.JpaRepository;
-import  com.setplex.middleware.domain.Package;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PackageRepo extends JpaRepository<Package, Integer> {
+
+    @Query("SELECT DISTINCT p FROM Package p JOIN FETCH p.channels")
+    public List<Package> findAllWithDeps();
 }
--- web/src/main/java/com/setplex/middleware/controller/PackageCtrl.java
+++ web/src/main/java/com/setplex/middleware/controller/PackageCtrl.java
@@ -1,12 +1,10 @@
 package com.setplex.middleware.controller;
 
-import com.setplex.middleware.model.PackageDTO;
+import com.setplex.middleware.domain.Package;
 import com.setplex.middleware.service.TVService;
-import  com.setplex.middleware.domain.Package;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 //@Transactional
+//@EnableHypermediaSupport(type = { EnableHypermediaSupport.HypermediaType.HAL })
 @RestController
 public class PackageCtrl {
 
     @Autowired
     TVService service;
 
-    @RequestMapping(value = "/packages", method = RequestMethod.GET)
+    @RequestMapping(value = "/packages", method = RequestMethod.GET /*produces="application/hal+json"*/)
     public List<Package> getPackages() {
         return service.getPackagesWithChannels();
     }
@@ -30,7 +29,7 @@ public class PackageCtrl {
     public Package getPackage(@PathVariable int id) {
         return service.getPackage(id);
     }
-
+//    @CrossOrigin
     @RequestMapping(value = "/packages", method = RequestMethod.POST)
     public ResponseEntity<Void> createPackage(Package pk) {
         service.createPackage(pk);
